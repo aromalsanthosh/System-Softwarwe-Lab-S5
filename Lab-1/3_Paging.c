1b10e9aa1fa95d1231f28481a245f571931e4dbf
aromalsanthosh <aromalponmana@gmail.com>
2021-11-23 19:00:47 +0530

    added comments

--- Lab-1/3_Paging.c
+++ Lab-1/3_Paging.c
@@ -3,20 +3,20 @@
 
 int main()
 { 
-    int np,ps,i;
-    int *sa;
+    int np,ps,i; //np = no of pages, ps = page size
+    int *sa; //start address
     
     printf("enter how many pages\n");
     scanf("%d",&np);
     printf("enter the page size \n");
     scanf("%d",&ps);
 
-    sa=(int*)malloc(2*np);
+    sa=(int*)malloc(2*np); //allocate memory for start address and end address
     
     for(i=0;i<np;i++)
     {
-        sa[i]=(long)malloc(ps);
-        printf("page%d\t address %u\n",i+1,sa[i]);
+        sa[i]=(long)malloc(ps); //allocate memory for pages
+        printf("page%d\t address %u\n",i+1,sa[i]); //print the start address of each page
     }
 
 }
