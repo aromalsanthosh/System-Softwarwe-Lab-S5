9fdf6c9de7ca709e234c1c525992f0a8e55f24f3
aromalsantosh <aromalponmana@gmail.com>
2021-11-30 09:07:17 +0530

    fix --bug

--- Lab-1/2b_Indexed.c
+++ Lab-1/2b_Indexed.c
@@ -7,8 +7,10 @@ int main()
     for(i=0;i<50;i++)
         f[i]=0;
     x:
+
     printf("enter index block\t");
     scanf("%d",&p);
+
     if(f[p]==0)
     {
         f[p]=1;
@@ -20,6 +22,9 @@ int main()
         printf("Block already allocated\n");
         goto x;
     }
+
+    y:
+    printf("\nEnter the file blocks to be indexed :");
     for(i=0;i<n;i++)
         scanf("%d",&inde[i]);
     for(i=0;i<n;i++)
@@ -27,7 +32,7 @@ int main()
         if(f[inde[i]]==1)
         {
             printf("Block already allocated");
-            goto x;
+            goto y;
         }
     }
     for(j=0;j<n;j++)
@@ -47,3 +52,18 @@ int main()
         exit(0);
     
 }
+
+
+/*
+OUTPUT:
+Enter index block 9
+Enter no of files on index 3
+1 2 3
+Allocated
+File indexed
+9-> 1:1
+9-> 2:1
+9->3:1
+Enter 1 to enter more files and 0 to exit
+*/
+
