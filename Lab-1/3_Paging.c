21668258c4ffd476d8e8b941862c1b1f38e16674
aromalsantosh <aromalponmana@gmail.com>
2021-12-13 21:36:29 +0530

    Replaced paging program

--- Lab-1/3_Paging.c
+++ Lab-1/3_Paging.c
@@ -1,25 +1,74 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<math.h>
+int frametable[50],pt = 0,frame_no;
 
-int main()
-{ 
-    int np,ps,i; //np = no of pages, ps = page size
-    int *sa; //start address
-    
-    printf("enter how many pages\n");
-    scanf("%d",&np);
-    printf("enter the page size \n");
-    scanf("%d",&ps);
-
-    sa=(int*)malloc(2*np); //allocate memory for start address and end address
-    
-    for(i=0;i<np;i++)
-    {
-        sa[i]=(long)malloc(ps); //allocate memory for pages
-        printf("page%d\t address %u\n",i+1,sa[i]); //print the start address of each page
-    }
-
+void paging(int page_no)
+{
+	int i;
+	if(pt + page_no > frame_no)
+	{
+		printf("Cannot store the program as the frame table is full \n");
+		return;
+	}
+	printf("Storing the program in frames of frame table\n");
+	for(i = pt;i<pt + page_no;i++)
+	{
+		frametable[i] = 1;
+		printf("%d -> %p \n",i,&frametable[i]);
+	}
+	pt = i;
+	printf("\n");
 }
+void main()
+{
+	int fs,pts,fts,page_no,status;
+	for(int j = 0;j<50;j++)
+	{
+		frametable[j] = 0;
+	}
+	printf("Enter the size of frame table in bytes\n");
+	scanf("%d",&fts);
+	printf("Enter the size of a frame in bytes \n");
+	scanf("%d",&fs);
+	frame_no = ceil(fts/fs);
+	printf("Total possible frames = %d \n",frame_no);
+	while(1)
+	{
+		printf("Enter 1 to store a program in the memory\nEnter 2 to exit\n");
+		scanf("%d",&status);
+		if(status == 1)
+		{
+			printf("Enter size of program in bytes\n");
+			scanf("%d",&pts);
+			page_no = ceil(pts/fs);
+			paging(page_no);
+		}
+		else
+			break;
+	}
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// int main()
+// { 
+//     int np,ps,i; //np = no of pages, ps = page size
+//     int *sa; //start address
+    
+//     printf("enter how many pages\n");
+//     scanf("%d",&np);
+//     printf("enter the page size \n");
+//     scanf("%d",&ps);
+
+//     sa=(int*)malloc(2*np); //allocate memory for start address and end address
+    
+//     for(i=0;i<np;i++)
+//     {
+//         sa[i]=(long)malloc(ps); //allocate memory for pages
+//         printf("page%d\t address %u\n",i+1,sa[i]); //print the start address of each page
+//     }
+
+// }
 
 /*
 Output:
