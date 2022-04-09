079ace482286eaca3addb1154b014c16043bc0d4
Aromal S <aromalsanthosh@protonmail.com>
2022-04-09 10:57:01 +0530

    Update 2c_Linked.c

--- Lab-1/2c_Linked.c
+++ Lab-1/2c_Linked.c
@@ -1,55 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
-        int i,f[50],st,len,p,a;
-        char ch;
-        for(i=0;i<50;i++)
-                f[i] = 0;
-      
-                printf("Enter the block to be allocated\n");
-                scanf("%d",&p);
-                
-                if(p!=0)
-                {
-                        
-                        for(i=0; i<p; i++)
-                        {
-                                
-                                scanf("%d",&a);
-                                f[a] = 1;
-                        }
-                }
-                else
-                {
-                        printf("No blocks allocated\n");
-                        
-                }
-          do
-          {
-                printf("Enter the starting\n");
-                scanf("%d",&st);
-                printf("Enter the length\n");
-                scanf("%d",&len);
-                for(i=st;i<(st+len);i++)
-                {
-                        if(f[i] == 0)
-                        {
-                                f[i] = 1;
-                                printf("%d -> %d\n",i,f[i]);
-                        }
-                        else
-                        {
-                                printf("Block already allocated\n");
-                        }
-                }
-                printf("Do you want to enter more (y/n)");
-                        scanf(" %c",&ch);
-        }while(ch == 'Y'|| ch=='y');
+int main() 
+{ 
+    int f[50],p,i,j,k,a,st,len,n,c;
+
+    for(i=0;i<50;i++)
+        f[i]=0; //initialize all blocks to 0 (unallocated)
+
+
+    printf("Enter how many blocks that are already allocated :");
+    scanf("%d",&p);
+    if(p!=0)
+    {
+        printf("\nEnter the blocks no.s that are already allocated :");
+        for(i=0;i<p;i++)
+        {
+            scanf("%d",&a);
+            f[a]=1; //allocate the blocks
+        }
+    }
+    
+    //X:
+
+    do{
+        printf("Enter the starting index block & length :");
+        scanf("%d%d",&st,&len);
+        k=len;
+
+        for(j=st;j<(k+st);j++)
+        {
+            if(f[j]==0)
+            {
+                f[j]=1;
+                printf("\n%d->%d",j,f[j]);
+            }
+            else
+            { 
+                printf("\n %d-  >file is already allocated",j);
+                k++;
+            }
+        }
+        
+        printf("\n If u want to enter one more file? (yes-1/no-0)");
+        scanf("%d",&c);
+    }while(c==1);
+    
 }
+
 /*
-Output:
-aromal@Aromal-Nitro-5:~/SS_Lab/System-Software-Lab-S5/Lab-1$ ./a.out 
+OUTPUT:
+aromal@Aromal-Nitro-5:~/labdemo$ ./a.out 
 Enter how many blocks that are already allocated :3
 
 Enter the blocks no.s that are already allocated :4 7 9
@@ -65,6 +65,5 @@ Enter the starting index block & length :3 7
 10->1
 11->1
 12->1
- If u want to enter one more file? (yes-1/no-0)
-0
+ If u want to enter one more file? (yes-1/no-0)0
 */
